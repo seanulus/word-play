05378058865f048298712a0b1b654e15a72b3657
Sean McDemrott <seanjmac@hotmail.com>
2018-03-06 19:49:08 -0800

    add jQuery to pick out words with 3 or more characters, reverse them and join them into a string

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,21 @@
 $(document).ready(function() {
   $("#sentences").submit(function(event) {
+    //debugger
 
+    var sentence = $("#sentence1").val();
+    var sentenceArray = sentence.split(" ");
+
+    var greaterArray = []
+
+    sentenceArray.map(function(words) {
+      if (words.length > 2) {
+        greaterArray.push(words)
+      };
+    });
+
+    finishedArray = greaterArray.reverse().join(" ");
+
+    $(".display").text(finishedArray);
 
     event.preventDefault();
   });
